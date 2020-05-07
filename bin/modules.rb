b91b327940c2a6d247f9348171e2350e9ce9d95b
Precious chiedu <udegbue69@gmail.com>
2020-05-07 12:41:12 +0100

    removed return statements

--- bin/modules.rb
+++ bin/modules.rb
@@ -1,54 +1,79 @@
-class LintError # rubocop:disable Style/Documentation, Style/FrozenStringLiteralComment
+class LintError # rubocop:disable Style/Documentation, FrozenStringLiteralComment
   def semicolon_error(lines)
+    warning = []
+    warnings = []
     lines.each_with_index do |line, index|
-      return 'warning: on line ' + (index + 1).to_s + ' do not use semicolons to terminate expressions' if line[-1] == ';' # rubocop:disable Layout/LineLength
-      return 'warning: empty trailing white space' if line[-1] == ' '
+      if line[-1] == ';'
+        warning.append('warning: on line ' + (index + 1).to_s + ' do not use semicolons to terminate expressions')
+        warnings.append('warning: empty trailing white space')
+      end
     end
+    warning
   end
+  # def semicolon_error(lines)
+  #   lines.each_with_index do |line, index|
+  #     return 'warning: on line ' + (index + 1).to_s + ' do not use semicolons to terminate expressions' if line[-1] == ';' # rubocop:disable Layout/LineLength
+  #     return 'warning: empty trailing white space' if line[-1] == ' '
+  #   end
+  # end
 
   def line_length_error(lines)
+    warning = []
     lines.each_with_index do |line, index|
-      return 'warning: line  ' + (index + 1).to_s + '  is too long' if line.size >= 80 # rubocop:disable Layout/LineLength
+      if line.size >= 80
+        warning.append('warning: line  ' + (index + 1).to_s + '  is too long')
+      end
     end
+    warning
   end
 
   def white_space(lines1)
-    lines1.each_with_index do |_line, index|
-      return 'warning: on line ' + (index + 1).to_s + ' first line identation of file_path' if lines1[0][0] == ' ' # rubocop:disable Layout/LineLength
-    end
+    warning = []
+      if lines1[0][0] == ' '
+        warning << 'warning: on line 1 first line identation of file_path'
+      end
+    warning
   end
 
   def first_line_blank(lines)
-    lines.each_with_index do |_line, index|
-      return 'warning: on line ' + (index + 1).to_s + ' unnecessary space at beginning' if lines[0].length.zero?  # rubocop:disable Layout/LineLength
-    end
+    warning = []
+      if lines[0].length.zero? 
+        warning << 'warning: on line 1 empty space at beginning'
+      end
+    warning
   end
 
   def missing_string_comment(lines)
-    lines.each_with_index do |_line, index|
-      return 'warning: on line ' + (index + 1).to_s + ' frozen literal string missing' if lines[0].length.zero?  # rubocop:disable Layout/LineLength
+    warning = []
+    if lines[0].length.zero? 
+      warning << 'warning: on line 1 frozen literal string missing'
     end
+    warning
   end
 
   def line_end(lines1)
-    return "warning: on line #{lines1.size} trailing blank lines" if lines1[-2].size.zero? # rubocop:disable Layout/LineLength
+    warning = []
+    if lines1[-2].size.zero?
+      warning.append("warning: on line #{lines1.size} trailing blank lines")
+    end
+    warning
   end
 
   def spacing_signs(lines)
+    warning = []
     lines.each_with_index do |line, index|
-      return 'warning: on line ' + (index + 1).to_s + ' remove extra space at the end' if line[-1] == ' ' # rubocop:disable Layout/LineLength
+      if line[-1] == ' '
+      warning << 'warning: on line ' + (index + 1).to_s + ' remove extra space at the end'
+      end
     end
+    warning
   end
 
   def end_line_space(lines)
-    return "after line #{lines.size} final newline missing" if lines.last.length.positive? # rubocop:disable Layout/LineLength
-  end
-
-  def wrong_combination(item1, item2)
-    return 'warning: cannot add string to integer' if item1.is_a?(String) && item2.is_a?(Integer) || if item2.is_a?(String) && item1.is_a?(Integer) # rubocop:disable Layout/LineLength
-                                                                                                     end # rubocop:disable Layout/LineLength
-
-    item1 + item2
+    warning = []
+    if lines.last.length.positive?
+      warning << "after line #{lines.size} final newline missing"
+    end
   end
 
   def run_linter(lines) # rubocop:disable Metrics/CyclomaticComplexity, Metrics/AbcSize, Metrics/MethodLength
--- spec/testing_spec.rb
+++ spec/testing_spec.rb
@@ -10,58 +10,49 @@ describe LintError do # rubocop:disable Metrics/BlockLength
 
   describe '#semicolon_error' do
     it 'if gives an error if a semicolon is used to end an expression' do
-      expect(test.semicolon_error(lines)).to eql('warning: on line 2 do not use semicolons to terminate expressions') # rubocop:disable Layout/LineLength
+      expect(test.semicolon_error(lines)).to eql(['warning: on line 2 do not use semicolons to terminate expressions']) # rubocop:disable Layout/LineLength
     end
   end
 
   describe '#line_length_error' do
     it 'return a warning of a lenghty line' do
-      expect(test.line_length_error(lines)).to eql('warning: line  4  is too long') # rubocop:disable Layout/LineLength
+      expect(test.line_length_error(lines)).to eql(['warning: line  4  is too long']) # rubocop:disable Layout/LineLength
     end
   end
 
   describe '#white_space' do
     it 'it return an error if the first character in the first  line is a space' do # rubocop:disable Layout/LineLength
-      expect(test.white_space(lines1)).to eql('warning: on line 1 first line identation of file_path') # rubocop:disable Layout/LineLength
+      expect(test.white_space(lines1)).to eql(['warning: on line 1 first line identation of file_path']) # rubocop:disable Layout/LineLength
     end
   end
 
   describe '#first_line_blank' do
     it 'returns a warning if the first line in a page is blank' do
-      expect(test.first_line_blank(lines)).to eql('warning: on line 1 unnecessary space at beginning') # rubocop:disable Layout/LineLength
+      expect(test.first_line_blank(lines)).to eql(['warning: on line 1 empty space at beginning']) # rubocop:disable Layout/LineLength
     end
   end
 
   describe '#missing_string_comment' do
     it 'returns a warning of missing literal string' do
-      expect(test.missing_string_comment(lines)).to eql('warning: on line 1 frozen literal string missing') # rubocop:disable Layout/LineLength
-    end
-  end
-
-  describe '#wrong_combination' do
-    it 'return a warning for adding a string to an integer' do
-      test = LintError.new
-      a = 'b'
-      b = 2
-      expect(test.wrong_combination(a, b)).to eql('warning: cannot add string to integer') # rubocop:disable Layout/LineLength
+      expect(test.missing_string_comment(lines)).to eql(['warning: on line 1 frozen literal string missing']) # rubocop:disable Layout/LineLength
     end
   end
 
   describe '#line_end' do
     it 'return a warning if there are blank lines below the last character' do
-      expect(test.line_end(lines1)).to eql('warning: on line 4 trailing blank lines') # rubocop:disable Layout/LineLength
+      expect(test.line_end(lines1)).to eql(['warning: on line 4 trailing blank lines']) # rubocop:disable Layout/LineLength
     end
   end
 
   describe '#spacing_signs' do
     it 'it returns a warning if there is an extra space at the end of an expression' do # rubocop:disable Layout/LineLength
-      expect(test.spacing_signs(lines)).to eql('warning: on line 5 remove extra space at the end') # rubocop:disable Layout/LineLength
+      expect(test.spacing_signs(lines)).to eql(['warning: on line 5 remove extra space at the end']) # rubocop:disable Layout/LineLength
     end
   end
 
   describe '#end_line_space' do
     it 'return a warning if there is no blank line after the last line with characters' do # rubocop:disable Layout/LineLength
-      expect(test.end_line_space(lines)).to eql('after line 5 final newline missing') # rubocop:disable Layout/LineLength
+      expect(test.end_line_space(lines)).to eql(['after line 5 final newline missing']) # rubocop:disable Layout/LineLength
     end
   end
 end
